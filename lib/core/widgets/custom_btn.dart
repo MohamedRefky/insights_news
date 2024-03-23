8ac071a094b0c64a23030e1c4030164979f14b70
Mohamedrifky1 <132143317+Mohamedrifky1@users.noreply.github.com>
2024-03-23 12:13:06 +0200

    f

---
+++ lib/core/widgets/custom_btn.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:insights_news/core/utils/coloes.dart';
+
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.width,
+    this.height,
+  });
+  final String text;
+  final Function() onPressed;
+  final double width;
+  final double? height;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height ?? 50,
+      width: width,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primary,
+              foregroundColor: AppColors.black,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10))),
+          onPressed: onPressed,
+          child: Text(text)),
+    );
+  }
+}
